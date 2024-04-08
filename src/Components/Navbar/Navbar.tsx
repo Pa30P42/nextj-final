29edb012f39ab3049eea90ab76f3a2ce9812a4c3
Pa30P42 <hajiev93@gmail.com>
2024-04-08 19:24:01 +0400

    add changes to text

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -6,6 +6,10 @@ const Navbar = () => {
     <div className={style.container}>
       <h2 className={style.title}>Main title</h2>
       <h2 className={style.title}>Main title</h2>
+      <p>
+        sit amet consectetur Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Nisi, sint.
+      </p>
     </div>
   )
 }
